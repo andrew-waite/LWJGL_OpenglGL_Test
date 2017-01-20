80cf4829302aa2c395c9bafd51a8f46024fbc0a5
Andrew Waite <drewrockz01@gmail.com>
2017-01-20 19:31:59 +1100

    Saving

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,18 +15,23 @@ public class Main
     
     private OBJLoader water;
     
-    public void init()
+    public void pre_init()
     {
         water = new OBJLoader("./OBJ_FILES/water.obj");
-        
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        GLU.gluLookAt(0, 0.0f, 10.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
-        GL11.glLoadIdentity();
     }
     
-    public void run()
+    public void init()
     {
-        init();
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, 800, 0, 600, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        GLU.gluLookAt(1, 0, 0, 0, 0, 0, 0, 1, 0);
+    }
+    
+    public void run() throws LWJGLException
+    {
+        pre_init();
         
         try
         {
@@ -38,6 +43,8 @@ public class Main
             e.printStackTrace();
         }
         
+        init();
+        
         while(!(Display.isCloseRequested()))
         {
             loop();
@@ -46,18 +53,68 @@ public class Main
         Display.destroy();
     }
     
-    public void loop()
+    public void loop() throws LWJGLException
     {
-        Display.update();
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);  
+        GL11.glClearColor(0f, 129.0f, 255.0f, 1.0f);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        // Reset transformations
+        GL11.glLoadIdentity();
+        
         draw();
+        Display.swapBuffers();
+        Display.update();  
     }
     
     public void draw()
     {
-       water.setColor(255, 255, 255).renderObject();
+        GL11.glColor3f(255,255,255);
+       //water.setColor(255, 255, 255).renderObject();
+        GL11.glBegin(GL11.GL_POLYGON);
+        GL11.glVertex3f(0.5f, -0.5f, 0.5f );
+        GL11.glVertex3f(0.5f,  0.5f, 0.5f );
+        GL11.glVertex3f(-0.5f,  0.5f, 0.5f );
+        GL11.glVertex3f(-0.5f, -0.5f, 0.5f );
+        GL11.glEnd();
+         
+        // Purple side - RIGHT
+        GL11.glBegin(GL11.GL_POLYGON);
+        //glColor3f(  1.0,  0.0,  1.0 );
+        GL11.glVertex3f(0.5f, -0.5f, -0.5f);
+        GL11.glVertex3f(0.5f,  0.5f, -0.5f);
+        GL11.glVertex3f(0.5f,  0.5f,  0.5f);
+        GL11.glVertex3f(0.5f, -0.5f,  0.5f);
+        GL11.glEnd();
+         
+        // Green side - LEFT
+        GL11.glBegin(GL11.GL_POLYGON);
+        //glColor3f(   0.0,  1.0,  0.0 );
+        GL11.glVertex3f(-0.5f, -0.5f,  0.5f);
+        GL11.glVertex3f(-0.5f,  0.5f,  0.5f);
+        GL11.glVertex3f(-0.5f,  0.5f, -0.5f);
+        GL11.glVertex3f(-0.5f, -0.5f, -0.5f);
+        GL11.glEnd();
+         
+        // Blue side - TOP
+        GL11.glBegin(GL11.GL_POLYGON);
+        //glColor3f(   0.0,  0.0,  1.0 );
+        GL11.glVertex3f(0.5f,  0.5f,  0.5f);
+        GL11.glVertex3f(0.5f,  0.5f, -0.5f);
+        GL11.glVertex3f(-0.5f,  0.5f, -0.5f);
+        GL11.glVertex3f(-0.5f,  0.5f,  0.5f);
+        GL11.glEnd();
+         
+        // Red side - BOTTOM
+        GL11.glBegin(GL11.GL_POLYGON);
+        //glColor3f(   1.0,  0.0,  0.0 );
+        GL11.glVertex3f(0.5f, -0.5f, -0.5f);
+        GL11.glVertex3f(0.5f, -0.5f,  0.5f);
+        GL11.glVertex3f(-0.5f, -0.5f,  0.5f);
+        GL11.glVertex3f(-0.5f, -0.5f, -0.5f);
+        GL11.glEnd();
     }
     
-    public static void main(String[] args)
+    public static void main(String[] args) throws LWJGLException
     {
         new Main().run();
     }
