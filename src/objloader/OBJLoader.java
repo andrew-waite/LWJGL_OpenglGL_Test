21fa36cf795586d8190e9d82796290a3bb01e09a
Andrew Waite <drewrockz01@gmail.com>
2017-01-20 16:30:32 +1100

    Add read and render loop sequence

--- src/objloader/OBJLoader.java
+++ src/objloader/OBJLoader.java
@@ -1,14 +1,56 @@
 package objloader;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lwjgl.opengl.GL11;
+
 public class OBJLoader
 {
-    public OBJLoader(String fileName)
+    List<Float> texCoords = new ArrayList<Float>();
+    List<Float> vertCoords = new ArrayList<Float>();
+    List<Float> faceCoords = new ArrayList<Float>();
+    
+    public OBJLoader(String fileName) throws IOException
     {
-        
+            BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+            
+            String currentLine;
+            
+            while((currentLine = fileReader.readLine()) != null)
+            {
+                if(currentLine.charAt(0) == 'f')
+                {
+                    String[] currentLineSplit = currentLine.split("/");
+                    
+                    texCoords.add(Float.valueOf(currentLineSplit[0]));
+                    texCoords.add(Float.valueOf(currentLineSplit[3]));
+                    texCoords.add(Float.valueOf(currentLineSplit[6]));
+                    
+                    vertCoords.add(Float.valueOf(currentLineSplit[1]));
+                    vertCoords.add(Float.valueOf(currentLineSplit[4]));
+                    vertCoords.add(Float.valueOf(currentLineSplit[6]));
+                    
+                    faceCoords.add(Float.valueOf(currentLineSplit[2]));
+                    faceCoords.add(Float.valueOf(currentLineSplit[5]));
+                    faceCoords.add(Float.valueOf(currentLineSplit[6]));
+                }
+            }
+            
+            fileReader.close();
     }
     
     public OBJLoader rotate(float rotationAngle)
     {
+        return this;
+    }
+    
+    public OBJLoader setColor(int r, int g, int b)
+    {
+        GL11.glColor3f(r, g, b);
         return this;
     }
     
@@ -24,6 +66,16 @@ public class OBJLoader
     
     public OBJLoader renderObjects()
     {
+        GL11.glPushMatrix();
+        GL11.glBegin(GL11.GL_TRIANGLES);
+        
+        for(int i = 0; i < faceCoords.size(); i += 3)
+        {
+            GL11.glVertex3f(faceCoords.get(i), faceCoords.get(i + 1), faceCoords.get(i + 2));
+        }
+        
+        GL11.glEnd();
+        GL11.glPopMatrix();
         return this;   
     }
 }
