961e668cf6ddc371fd78e69d210d4dfcb6bb854d
Andrew Waite <drewrockz01@gmail.com>
2017-01-20 15:02:12 +1100

    Screen setup and simple 2D triangle drawn for testing

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,52 @@
 package main;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
 public class Main
 {
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 600;
+    
     public void run()
     {
+        try
+        {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create();
+        } 
+        catch (LWJGLException e)
+        {
+            e.printStackTrace();
+        }
+        
+        while(!(Display.isCloseRequested()))
+        {
+            loop();
+        }
+        
+        Display.destroy();
     }
+    
+    public void loop()
+    {
+        Display.update();
+        draw();
+    }
+    
+    public void draw()
+    {
+        GL11.glPushMatrix();
+        GL11.glBegin(GL11.GL_TRIANGLES);
+        GL11.glVertex2f(0,0);
+        GL11.glVertex2f(0.5f,1);
+        GL11.glVertex2f(1,0);
+        GL11.glEnd();
+        GL11.glPopMatrix();
+    }
+    
     public static void main(String[] args)
     {
         new Main().run();
