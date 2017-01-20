e2e9be3e6f2b1e504971168f4dbd4b50f9042904
Andrew Waite <drewrockz01@gmail.com>
2017-01-20 15:15:14 +1100

    Base OBJLoader built framework

---
+++ src/objloader/OBJLoader.java
@@ -0,0 +1,29 @@
+package objloader;
+
+public class OBJLoader
+{
+    public OBJLoader(String fileName)
+    {
+        
+    }
+    
+    public OBJLoader rotate(float rotationAngle)
+    {
+        return this;
+    }
+    
+    public OBJLoader translate(float x, float y, float z)
+    {
+        return this;
+    }
+    
+    public OBJLoader scale(float x, float y, float z)
+    {
+        return this;
+    }
+    
+    public OBJLoader renderObjects()
+    {
+        return this;   
+    }
+}
